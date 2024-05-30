f9bf4ebebe4cc0d66c0a864f7e5e79ae851fc90e
Electroneuron24 <electroneuron.edu@gmail.com>
2024-05-30 17:55:25 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+let popup = document.getElementsByClassName("popup-card")[0];
+let closeBtn = document.getElementById("close-btn");
+
+function showPopup() {
+  setTimeout(() => {
+    popup.style.transform = "translateY(0)";
+  }, 3000);
+}
+
+closeBtn.addEventListener("click", (e) => {
+  popup.style.transform = "translateY(-70vh)";
+});
+
+window.onload = showPopup();
+
 /*more details button*/
 const container = document.querySelector(".info-box");
 const toggle = document.querySelector(".toggle");
